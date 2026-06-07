90e7c1f0f5a49cba0c65a49a0d67f6724d675925
HamzaAhmed356 <hamzaahmed121.171@gmail.com>
2026-06-07 12:02:35 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import { Server } from "socket.io";
+
+const io = new Server(3002, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userData, socketId) => {
+  const exists = users.find((user) => user.sub === userData.sub);
+
+  if (!exists) {
+    users.push({ ...userData, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+const getUser = (userId) => {
+  return users.find((user) => user.sub == userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  socket.on("addUsers", (userData) => {
+    addUser(userData, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+    console.log("User Disconnected:", socket.id);
+  });
+  socket.on("sendMessage", (data) => {
+    const user = getUser(data.receiverId);
+    io.to(user.socketId).emit("getMessage", data);
+  });
+});
